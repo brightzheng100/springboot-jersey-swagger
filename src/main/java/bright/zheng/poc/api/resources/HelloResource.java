d5420c3b4ef365020fd5795812bdad8d4c54c396
Bright Zheng <bright.zheng@outlook.com>
2016-06-21 18:06:43 +0800

    enhanced the annotation for @Path at class level

--- src/main/java/bright/zheng/poc/api/resources/HelloResource.java
+++ src/main/java/bright/zheng/poc/api/resources/HelloResource.java
@@ -21,7 +21,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @Component
-@Path("/v1")
+@Path("/v1/hello")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Api(value = "Hello API - say hello the world", produces = "application/json")
@@ -30,7 +30,7 @@ public class HelloResource {
 	private static final Logger LOGGER = LoggerFactory.getLogger(HelloResource.class);
 
 	@GET						//JAX-RS Annotation
-	@Path("hello/{name}")	//JAX-RS Annotation
+	@Path("/{name}")	//JAX-RS Annotation
 	@ApiOperation(				//Swagger Annotation
 			value = "Say hello by providing the name by URI, via standard json header", 
 			response = Hello.class)  
